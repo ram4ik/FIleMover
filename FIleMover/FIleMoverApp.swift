6a2241de98a603682408cebf448a1d8b033cc814
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-25 23:23:31 +0300

    Initial Commit

---
+++ FIleMover/FIleMoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIleMoverApp.swift
+//  FIleMover
+//
+//  Created by Ramill Ibragimov on 25.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct FIleMoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
